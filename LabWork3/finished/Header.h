cab780bdcf1aaa3662dcb4be0fbbd648488ddafd
LeviiValenok <74707310+LeviiValenok@users.noreply.github.com>
2021-04-14 10:05:13 +0300

    Update Header.h

--- LabWork3/finished/Header.h
+++ LabWork3/finished/Header.h
@@ -7,10 +7,17 @@
 
 #endif //LABWORK3_HEADER_H
 #include <stdio.h>
+#include <stdbool.h>
+
+enum fileType
+{
+    BINARYFILE = 1,
+    TEXTFILE = 2
+};
 
 void mainMenu(FILE* textFilePointer);
 void menuForTextFile(FILE* textFilePointer);
-int inputValidation();
+int inputValidation(bool type);
 int menu();
 int menuForTextChoice();
 void inputStrings(char* nameOfFile);
@@ -19,3 +26,12 @@ void seekAndOutputSuitableItem(int number, char* nameOfFile);
 void reverseWord(char* fileName, int wordLength, int endPosition);
 void findByItem(char* nameOfFile, int number);
 void outputWord(char* nameOfFile, int wordSize, int place);
+int menuForBinaryChoice();
+void inputIntegers(char* nameOfTheFile);
+void outputIntegers(char* nameOfTheFile);
+void menuForBinaryFile();
+int findNumbers(char* nameOfTheFile);
+char* enterFileName(enum fileType type, char* fileName);
+void enterIntegerOnDescending(char* nameOfFile);
+int menuForSortFiles();
+void combineTwoFilesInDescendingOrder(char* firstFile, char* secondFile, char* resultFile);
